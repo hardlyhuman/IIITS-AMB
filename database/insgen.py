1a2839c63318227c18087bb1a7f2dcdcf6aacfa9
DongLee <sriharsha.g15@iiits.in>
2017-02-25 01:11:46 +0530

    Add files via upload

---
+++ database/insgen.py
@@ -0,0 +1,36 @@
+import random, string
+
+def randomword(length):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    m = ''
+    for i in range(0,length):
+        m += random.choice(alpha)
+        
+    return m
+
+def random_mno():
+    number = '0123456789'
+    mno = ''
+    for i in range(0,10):
+        mno += random.choice(number)
+        
+    return mno
+
+pr = ['TA', 'UTTA', 'Prof']
+ge = ['m', 'f', 'o']
+idd=101
+for i in range(0,40):
+    name = randomword(5)
+    email = name + "@iiits.in"
+    passw = "*C052D05DCC24DC02C52CAF02D77345ED50E43AC4"
+    mob = random_mno()
+
+    course = random.randint(0,25)
+    status = '0'
+    k=random.randint(0,2)
+    gender = ge[k]
+    program = pr[k]
+
+#print ("("+"'"+str(idd)+"', '"+name+"', '"+email+"', '"+passw+"', '"+mob+"', '"+gender+"', '"+program+"', '"+course+"', '"+str(status)+")"+",")
+    print ("("+"'" , end=''), print (idd,end=''), print("', '"+name+"', '"+email+"', '"+passw+"', '",end=''),print (mob,end=''),print ("', '",end=''), print (gender,end=''), print("', '",end=''),print(program,end=''),print("', '",end=''),print(course,end=''),print("', '",end=''),print (status,end=''),print("')"+",")
+    idd+=1
