d11b019986447620c9736180a6e8a247878d9f0c
DongLee <sriharsha.g15@iiits.in>
2017-02-25 01:26:11 +0530

    Add files via upload

---
+++ popupbox/my_js.js
@@ -0,0 +1,10 @@
+
+//document.getElementById('form').submit();
+//Function To Display Popup
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
